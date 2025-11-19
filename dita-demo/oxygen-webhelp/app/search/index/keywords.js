fe0ba0859c393be91685d5d906032e6d3bd9ccb9
Ian Mayo <ian@planetmayo.com>
2025-11-19 10:34:33 +0000

    update dita demo

--- dita-demo/oxygen-webhelp/app/search/index/keywords.js
+++ dita-demo/oxygen-webhelp/app/search/index/keywords.js
@@ -1,4 +1,4 @@
-var keywords=[{w:"Background",p:["p0"]},{w:"Security",p:["p1"]},{w:"7",p:["p2"]},{w:"Questions",p:["p2","p11","p12","p13"]},{w:"Detailed",p:["p3"]},{w:"description",p:["p3"]},{w:"of",p:["p3","p7"]},{w:"some",p:["p3"]},{w:"event",p:["p3"]},{w:"Contact",p:["p4"]},{w:"Classification",p:["p4"]},{w:"Analysis",p:["p4","p6","p7","p8","p9","p10"]},{w:"Tactical",p:["p5"]},{w:"Decision",p:["p5"]},{w:"Matrix",p:["p5"]},{w:"Gram",p:["p6","p7","p8"]},{w:"1",p:["p6","p9","p16"]},{w:"2",p:["p7","p10"]},{w:"-",p:["p7"]},{w:"Spirit",p:["p7"]},{w:"Whale",p:["p7"]},{w:"Island",p:["p7"]},{w:"3",p:["p8"]},{w:"Gramless",p:["p9","p10"]},{w:"MCQ",p:["p11"]},{w:"Only",p:["p11","p13"]},{w:"Mixed",p:["p12"]},{w:"Quiz",p:["p12"]},{w:"Numeric",p:["p13"]},{w:"Context",p:["p14"]},{w:"Sensitive",p:["p14"]},{w:"Help",p:["p14"]},{w:"Page",p:["p15","p16"]},{w:"Index",p:["p15"]},{w:"Cover",p:["p16"]}];
+var keywords=[{w:"Background",p:["p0"]},{w:"Security",p:["p1"]},{w:"7",p:["p2"]},{w:"Questions",p:["p2","p11","p12","p13"]},{w:"Detailed",p:["p3"]},{w:"description",p:["p3"]},{w:"of",p:["p3","p7"]},{w:"some",p:["p3"]},{w:"event",p:["p3"]},{w:"Contact",p:["p4"]},{w:"Classification",p:["p4"]},{w:"Analysis",p:["p4","p6","p7","p8","p9","p10"]},{w:"Tactical",p:["p5"]},{w:"Decision",p:["p5"]},{w:"Matrix",p:["p5"]},{w:"Gram",p:["p6","p7","p8"]},{w:"1",p:["p6","p9","p16"]},{w:"2",p:["p7","p10"]},{w:"-",p:["p7"]},{w:"Spirit",p:["p7"]},{w:"Whale",p:["p7"]},{w:"Island",p:["p7"]},{w:"3",p:["p8"]},{w:"Gramless",p:["p9","p10"]},{w:"MCQ",p:["p11"]},{w:"Only",p:["p11","p13"]},{w:"Mixed",p:["p12"]},{w:"Quiz",p:["p12"]},{w:"Numeric",p:["p13"]},{w:"Context",p:["p14"]},{w:"Sensitive",p:["p14"]},{w:"Help",p:["p14"]},{w:"Assessment",p:["p15"]},{w:"Cover",p:["p16"]},{w:"Page",p:["p16"]}];
 var ph={};
 ph["p0"]=[0];
 ph["p1"]=[1];
@@ -15,8 +15,8 @@ ph["p12"]=[26, 27, 3];
 ph["p11"]=[24, 3, 25];
 ph["p14"]=[29, 30, 31];
 ph["p13"]=[28, 3, 25];
-ph["p16"]=[34, 32, 16];
-ph["p15"]=[32, 33];
+ph["p16"]=[33, 34, 16];
+ph["p15"]=[32];
 var keywordsInfo = {
     keywords: keywords,
     ph: ph
